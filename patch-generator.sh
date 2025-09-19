255406c0845daa7d28168e589e0ce7a6255ff100
Xarblu <xarblu@protonmail.com>
2025-09-19 13:47:38 +0200

    refactor patch generator

--- patch-generator.sh
+++ patch-generator.sh
@@ -8,39 +8,47 @@
 #   - origin -> git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 #   - bcachefs -> git://evilpiepirate.org/bcachefs.git
 
+# git refs used
+declare -g LINUX_REF='origin/master'
+declare -g BCACHEFS_REF='bcachefs/for-next'
+
+# log utility function writing to stderr
 function log() {
     # shellcheck disable=SC2059
     printf "${@}" 1>&2
 }
 
+# Y/n prompt
 function confirm() {
     local prompt="${1:-'Confirm?'}"
     local response
     while true; do
         read -r -p "${prompt} [Y/n]: " response
         case "${response,,}" in
-            y|yes|"") return 0 ;;
+            y|yes|'') return 0 ;;
             n|no) return 1 ;;
-            *) log "Bad response\n" ;;
+            *) log 'Bad response\n' ;;
         esac
     done
 }
 
+# --help listing
 function help() {
-    log "Usage:\n"
-    log "  -o|--output  Output file or directory\n"
-    log "               If file (.patch extension) this exact file is used\n"
-    log "               If directory (must exist) auto generate a patch\n"
-    log "               If not given auto generates file in current directory\n"
+    log 'Usage:\n'
+    log '  -o|--output  Output file or directory\n'
+    log '               If file (.patch extension) this exact file is used\n'
+    log '               If directory (must exist) auto generate a patch\n'
+    log '               If not given auto generates file in current directory\n'
 }
 
+# argparser
 function parse_args() {
     while (( ${#} > 0 )); do
         case "${1}" in
             -o|--output)
                 if (( ${#} < 2 )); then
-                    log "Expected argument after %s\n" "${1}"
-                    return 1
+                    log 'Expected argument after %s\n' "${1}"
+                    exit 1
                 fi
                 OUT_FILE="${2}"
                 shift 2
@@ -50,29 +58,36 @@ function parse_args() {
                 exit 0
                 ;;
             *)
-                log "Bad argument %s\n" "${1}"
-                return 1
+                log 'Bad argument %s\n' "${1}"
+                exit 1
                 ;;
         esac
     done
 }
 
+# basic checks if repo is valid
 function check_repo() {
     # basic check if we're in a git repo
     if [[ ! -d .git ]]; then
-        log ".git does not exist\n"
-        return 1
+        log '.git does not exist\n'
+        log 'Is the current directory a linux source tree?\n'
+        exit 1
     fi
 
     # check if we have required remotes
-    local remote
-    for remote in origin bcachefs; do
-        if ! git remote get-url "${remote}"; then
-            log "Expected remote %s does not exist\n" "${remote}"
+    local remote url
+    for remote in "${LINUX_REF%/*}" "${BCACHEFS_REF%/*}"; do
+        if url=$(git remote get-url "${remote}"); then
+            log 'Using remote %s from %s\n' "${remote}" "${url}" 
+        else
+            log 'Expected remote %s does not exist\n' "${remote}"
+            exit 1
         fi
     done
 }
 
+# get last git tag name before the checked-out commit
+# returned in REPLY
 function last_tag() {
     declare -g _LAST_TAG
 
@@ -80,12 +95,28 @@ function last_tag() {
         _LAST_TAG="$(git describe --abbrev=0)"
     fi
 
-    printf "%s" "${_LAST_TAG}"
+    REPLY="${_LAST_TAG}"
 }
 
+# get date of last commit formatted as
+# YYYYmmDDHHMMSS
+# returned in REPLY
+function get_ref_date() {
+    local ref="${1}"
+    local unix_time
+    if ! unix_time="$(git log -n 1 --format=%ct "${ref}")"; then
+        log 'Bad ref: %s\n' "${ref}"
+        exit 1
+    fi
+
+    TZ=UTC0 printf -v REPLY '%(%Y%m%d%H%M%S)T' "${unix_time}"
+}
+
+# generate output file name
+# returned in REPLY
 function out_file() {
     if [[ "${OUT_FILE}" == *.patch ]]; then
-        printf '%s' "${OUT_FILE}"
+        REPLY="${OUT_FILE}"
         return 0
     fi
 
@@ -94,50 +125,59 @@ function out_file() {
     fi
 
     if [[ -d "${OUT_FILE}" ]]; then
-        OUT_FILE="${OUT_FILE%/}/bcachefs-$(TZ=UTC date '+%Y%m%d%H%M%S')-for-$(last_tag).patch"
+        get_ref_date "${BCACHEFS_REF}"
+        local bch_date="${REPLY}"
+        last_tag
+        local lnx_tag="${REPLY}"
+        OUT_FILE="${OUT_FILE%/}/bcachefs-${bch_date}-for-${lnx_tag}.patch"
     else
-        log "Output %s does not end with .patch and is not an existing directory" \
+        log 'Output %s does not end with .patch and is not an existing directory\n' \
             "${OUT_FILE}"
         exit 1
     fi
 
-    printf '%s' "${OUT_FILE}"
+    REPLY="${OUT_FILE}"
 }
 
 function main() {
-    if ! parse_args "${@}"; then
-        help
-        return 1
+    parse_args "${@}"
+
+    check_repo
+
+    if confirm "Fetch ${LINUX_REF%/*}?"; then
+        git fetch "${LINUX_REF%/*}"
     fi
 
-    if confirm "Fetch origin?"; then
-        git fetch origin
+    if confirm "Reset repo to ${LINUX_REF}?"; then
+        git reset --hard "${LINUX_REF}"
     fi
 
-    if confirm "Reset repo to master?"; then
-        git reset --hard origin/master
+    last_tag
+    local tag="${REPLY}"
+    log 'Detected last tag %s\n' "${tag}"
+
+    if confirm 'Reset repo to that tag?'; then
+        git reset --hard "${tag}"
     fi
 
-    log "Detected last tag %s\n" "$(last_tag)"
-
-    if confirm "Reset repo to that tag?"; then
-        git reset --hard "$(last_tag)"
-    fi
-
-    if confirm "Cleanup any stale files/directories?"; then
+    if confirm 'Cleanup any stale files/directories?'; then
         git clean -fdx
     fi
 
-    if confirm "Fetch bcachefs?"; then
-        git fetch bcachefs
+    if confirm "Fetch ${BCACHEFS_REF%/*}?"; then
+        git fetch "${BCACHEFS_REF%/*}"
     fi
 
-    if confirm "Merge bcachefs/master?"; then
-        git merge bcachefs/master
+    if confirm "Merge ${BCACHEFS_REF}?"; then
+        git merge "${BCACHEFS_REF}"
     fi
 
-    if confirm "Write patch to $(out_file)"; then
-        git diff 'HEAD~' > "$(out_file)"
+    out_file
+    local file="${REPLY}"
+
+    if confirm "Write patch to ${file}"; then
+        git diff 'HEAD~' > "${file}"
     fi
 }
+
 main "${@}"
