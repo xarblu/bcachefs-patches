5e20a9a5f0157a8769d58b75986e17889501525c
Xarblu <xarblu@protonmail.com>
2025-10-13 21:07:49 +0200

    6.18: add glue for namei.c changes
    Based on https://github.com/torvalds/linux/commit/3d18f80ce181ba27f37d0ec1c550b22acb01dd49

--- patch-generator.sh
+++ patch-generator.sh
@@ -230,6 +230,7 @@ function glue_patch() {
                     glue+=(
                         "${script_dir}/6.18/bcachefs-glue-v1.31.7-kvmalloc_noprof.patch"
                         "${script_dir}/6.18/bcachefs-glue-v1.31.7-bio_init.patch"
+                        "${script_dir}/6.18/bcachefs-glue-v1.31.7-namei_c-renames.patch"
                     )
                     ;;
             esac
